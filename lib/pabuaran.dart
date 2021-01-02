277bf31b44f43ba81e9f8de8615f6986fddca2bc
user1 <user1@email.com>
2021-01-03 05:04:41 +0700

    publish

---
+++ lib/pabuaran.dart
@@ -0,0 +1,11 @@
+library pabuaran;
+
+import 'package:flutter/material.dart';
+
+part 'src/buttonUngu.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/src/buttonUngu.dart
@@ -0,0 +1,17 @@
+part of '../pabuaran.dart';
+
+class ButtonUngu extends StatelessWidget {
+  final Function onPressed;
+  final String text;
+
+  ButtonUngu({@required this.onPressed, @required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: this.onPressed,
+      child: Text(this.text),
+      color: Colors.purpleAccent,
+    );
+  }
+}
